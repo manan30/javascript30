f63d7551ffbb69a7246c549e5c8cb19c4435f1fa
Manan Joshi <mananjoshi1995@gmail.com>
2020-07-29 12:22:17 -0400

    Adding functionality for forward and backward buttons

--- 11-custom-html5-video-player/index.js
+++ 11-custom-html5-video-player/index.js
@@ -1,10 +1,26 @@
 const videoElement = document.querySelector('video');
 let isPlaying = false;
 
+const forward25Button = document.querySelector('.forward-25');
+const backward10Button = document.querySelector('.backward-10');
+
 videoElement.addEventListener('click', () => {
   if (videoElement.paused) {
+    console.dir(videoElement);
     videoElement.play();
   } else {
     videoElement.pause();
+  }
+});
+
+forward25Button.addEventListener('click', () => {
+  if (!videoElement.paused) {
+    videoElement.currentTime += 25;
+  }
+});
+
+backward10Button.addEventListener('click', () => {
+  if (!videoElement.paused) {
+    videoElement.currentTime -= 10;
   }
 });
