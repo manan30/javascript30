a70d9f2fc7eed4681b3b900bf2cd8e4c9e679da8
Manan Joshi <mananjoshi1995@gmail.com>
2020-07-29 16:02:00 -0400

    Completing all required functionality

--- 11-custom-html5-video-player/index.js
+++ 11-custom-html5-video-player/index.js
@@ -1,22 +1,50 @@
 const videoElement = document.querySelector('video');
-let isPlaying = false;
-
+const progressBarContainer = document.querySelector('.progress-bar-container');
+const progressBar = document.querySelector('.progress-bar');
 const playPauseButton = document.querySelector('.play-pause-button');
+const sliders = document.querySelectorAll('input[type=range]');
 const forward25Button = document.querySelector('.forward-25-button');
 const backward10Button = document.querySelector('.backward-10-button');
+let mouseDown = false;
 
 const handlePlayPauseStateChange = () => {
   if (videoElement.paused) {
-    // console.dir(videoElement);
     videoElement.play();
-    playPauseButton.innerHTML = 'Pause';
   } else {
     videoElement.pause();
-    playPauseButton.innerHTML = 'Play';
   }
 };
 
+const handlePlayPauseButtonUpdate = () => {
+  if (videoElement.paused) {
+    playPauseButton.textContent = 'Play';
+    videoElement.classList.remove('video-hover');
+  } else {
+    playPauseButton.textContent = 'Pause';
+    videoElement.classList.add('video-hover');
+  }
+};
+
+function handleSliderUpdate() {
+  videoElement[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (videoElement.currentTime / videoElement.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime =
+    (e.offsetX / progressBarContainer.offsetWidth) * videoElement.duration;
+  videoElement.currentTime = scrubTime;
+}
+
 videoElement.addEventListener('click', handlePlayPauseStateChange);
+videoElement.addEventListener('play', handlePlayPauseButtonUpdate);
+videoElement.addEventListener('pause', handlePlayPauseButtonUpdate);
+videoElement.addEventListener('timeupdate', handleProgress);
+
 playPauseButton.addEventListener('click', handlePlayPauseStateChange);
 
 forward25Button.addEventListener('click', () => {
@@ -30,3 +58,16 @@ backward10Button.addEventListener('click', () => {
     videoElement.currentTime -= 10;
   }
 });
+
+sliders.forEach((slider) => {
+  slider.addEventListener('change', handleSliderUpdate);
+  slider.addEventListener('mousemove', handleSliderUpdate);
+});
+
+progressBarContainer.addEventListener('click', scrub);
+progressBarContainer.addEventListener('mousedown', () => (mouseDown = true));
+progressBarContainer.addEventListener('mouseup', () => (mouseDown = false));
+progressBarContainer.addEventListener(
+  'mousemove',
+  (e) => mouseDown && scrub(e)
+);
